251610707b573b089b243bfb4e84211c7d4cbb1a
Hugo Obuchi <76051977+hugo-obuchi@users.noreply.github.com>
2021-09-21 09:19:22 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AGLW_CSharp_BetterChatClientSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
